0c6db1aaad48e689d1bc599b4613d81a54536496
Ricardo Matos <ricardothadeu8@outlook.com>
2022-07-03 00:54:09 -0300

    Add media query

--- script.js
+++ script.js
@@ -4,20 +4,34 @@ let container = document.getElementsByClassName("container")[0];
 let socials = document.getElementsByClassName("socials-share")[0];
 let icon = document.getElementsByClassName("icon")[0];
 let change = true
-
+let width = window.innerWidth;
 
 share.addEventListener("click", () => {
-        if(change == true) {
+    if(change == true) {
+        
+        icon.setAttribute("fill", "#ffffff");
+        share.setAttribute("style","background-color: hsl(214, 17%, 51%);")
+        change = false;
+        
+        if(width < 1024) {
             container.setAttribute("style", "height: 30.5rem;");
             socials.removeAttribute("style", "display:none;");
-            icon.setAttribute("fill", "#ffffff");
-            share.setAttribute("style","background-color: hsl(214, 17%, 51%);")
-            change = false;
-        } else {
+        }
+        else {
+            socials.removeAttribute("style", "display:none;");
+        }
+
+    } else {
+        if(width < 1024) {
             container.setAttribute("style", "height: 31.5rem;");
             socials.setAttribute("style", "display:none;");
-            icon.setAttribute("fill", "#6E8098");
-            share.setAttribute("style","background-color: hsl(210, 46%, 95%);")
-            change = true;
         }
-    });
+        else {
+            socials.setAttribute("style", "display:none;");
+        }
+        
+        icon.setAttribute("fill", "#6E8098");
+        share.setAttribute("style","background-color: hsl(210, 46%, 95%);")
+        change = true;
+    }
+});
